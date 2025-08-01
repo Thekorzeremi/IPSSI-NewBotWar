47d104c11b61150977035dd828ea271feb60fe64
Rémi K <korzeremi02+zxpywci6@outlook.com>
2025-08-01 11:00:37 +0200

    cicd: added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.disable('etag');
