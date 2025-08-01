5f5e56d1c0b8de4d23eabab84b0cf25147146496
Rémi K <korzeremi02+zxpywci6@outlook.com>
2025-08-01 09:42:57 +0200

    init: new version of botwar, no x-state

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/control', (req,res) => {
+    fs.writeFileSync('lastCommand.json', JSON.stringify(req.body));
+    res.send({ success: true });
+});
+
+app.get('/action', (req,res) => {
+    const raw = fs.readFileSync('lastCommand.json');
+    const command = JSON.parse(raw);
+    res.json(command);
+});
+
+app.listen(PORT, () => {
+    console.log(`Le bot est lancé sur http://127.0.0.1:${PORT}`);
+});
